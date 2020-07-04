e84245f70c33507da509f1bcbbd6924e1848fc6e
Greg Mangan <mangangreg@gmail.com>
2020-07-04 13:30:32 -0400

    Initial commit

---
+++ code/tools/settings.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+PROJECT_PATH = Path('../../').absolute()
+DATA_PATH = PROJECT_PATH / 'data'
---
+++ code/tools/tools.py
@@ -0,0 +1,33 @@
+def gen_id(wiki_url):
+    ''' Generate an 8-digit id (int) for based on the unique wikipedia url'''
+    return hash(wiki_url) % 10**8
+
+def clean_text(string):
+    ''' Clean common cell text from a <td> tag '''
+    return string.replace('\xa0', ' ').replace('\n',' ').strip()
+
+def missing_link(name, soup):
+    ''' Find a hpyerlink for a given name '''
+    if soup.find('a', text=name):
+        return soup.find('a', text=name).attrs.get('href')
+    else:
+        return None
+
+def get_hyper_list(td, soup):
+    '''
+        Extract names and URLs from horizontal lists of hyperlinks
+
+        Inputs:
+            td (bs4 tag)
+    '''
+    result_list = []
+    if not td.find('ul'):
+        return [{'name': td.text, 'url':None, 'order': 0}]
+    else:
+        result_list = []
+        for i, li in enumerate(td.find_all('li')):
+            name = li.text
+            url = li.find('a').attrs.get('href') if li.find('a') else missing_link(name, soup)
+            result_list.append({'name': name, 'url': url, 'order': i})
+
+        return result_list
